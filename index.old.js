b52a364ac6ef8a519fd810fbca8d5d6fc2ea0e50
Danielo Rodriguez <rdanielo@gmail.com>
2020-08-24 23:46:41 +0200

    chore: reduce the amount of stuff on JS side

---
+++ index.old.js
@@ -0,0 +1,33 @@
+const USAGE = "_usage";
+const ignoreSheets = [USAGE, "Materials"]
+
+const normalizeStr = str => str.toLowerCase().replace(/ /g, '_')
+const flatten = arr => arr.reduce((a, b) => [...a, ...b], [])
+const indexArr = arr => Object.assign({}, arr)
+const arrToObj = (obj, k) => ({ ...obj, [k]: [] })
+const isEmptyArr = arr => arr.every(i => i)
+const not = f => (x) => !f(x)
+
+
+const getSheetData = sheet => sheet.getDataRange().getValues()
+const getSheetAsObj = sheet => {
+  const data = (getSheetData(sheet)).filter(not(isEmptyArr))
+  const lookup = indexArr(data[0])
+  const result = data[0].reduce(arrToObj, {})
+
+  Logger.log(Util.sumSheets(data))
+  return data.slice(1).reduce((res, row) => {
+    row.forEach((val, idx) => res[lookup[idx]].push(val))
+    return res
+  }, result)
+}
+
+function myFunction() {
+  s = SpreadsheetApp.getActiveSpreadsheet()
+  const usageSheetData = s.getSheetByName(USAGE)
+  const allSheets = s.getSheets()
+  const sheets = allSheets.filter(sheet => !ignoreSheets.includes(sheet.getName()))
+  // Logger.log(JSON.stringify(sheets.map(getSheetData)))
+  const allData = sheets.map(getSheetAsObj)
+  Logger.log(allData)
+}
